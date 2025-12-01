0f88ce2f1e0d85cfa180ad41849d45cc676b1038
Pratiksha Bhadalkar <bhadlkarpratiksha@gmail.com>
2025-12-01 11:17:59 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,87 @@
+/*⭐ 1. What is a Function? (Super Easy Explanation)
+
+Function = A reusable block of code.
+You write the code once and use it again and again.*/
+
+//⭐ 2. How to Write a Function? (Normal Function)
+//Example 1: Add two numbers
+
+
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(3,4));
+//output-07
+
+//⭐ 3. Arrow Function (Short Form)
+// const add = (a,b)=>{return a+b;
+// };
+
+
+//or  shorter 
+//const add = (a,b)=>a+b
+
+// ⭐ 4. Function with Conditions
+// Check even or odd:
+
+// function  checkEvenOdd(num)
+// {
+//     if(num%2==0){
+//         return "EVEN";
+//     }
+//     else{
+//        return "ODD";  
+//     }
+
+// }
+
+//⭐ 5. Function Returning String
+//Full name function:
+
+// function fullName(first,last){
+//     return first+" "+last;
+// }
+// console.log(fullName("pratiksha","bhadalkar"))
+
+//⭐ 6. Function + Array
+//Filter numbers greater than 10:
+// function filterGreaterThanTen (arr){
+
+//     return arr.filter(num=>num>10);
+// }
+// console .log(filterGreaterThanTen([1,90,10,45,56,78]))
+
+/* 1️⃣ Write a function to multiply two numbers
+
+function multiply(a,b){
+return a*b;
+}
+console.log(multiply(3,4));
+2️⃣ Write a function to check if age is 18+
+function  checkAge(age){
+if(age>=18)
+{
+return "age  is  18+"}
+} 
+else{
+    return "age  is not 18+"
+    }
+}
+    console.log(checkAge(20))
+3️⃣ Write a function to convert minutes into seconds
+
+function convertMinutesToSeconds(min) {
+  return min * 60;
+}
+
+console.log(convertMinutesToSeconds(5));  // 300
+
+4️⃣ Write a function to return the length of a string
+
+
+function  length(str){
+return str.length
+}
+console.log(length("pratiksha"))
+
+*/
